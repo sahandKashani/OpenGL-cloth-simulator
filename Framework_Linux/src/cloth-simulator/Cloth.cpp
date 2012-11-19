3115b3ec6dde532da985c12328cff99a596f32fc
Sahand Kashani-Akhavan <sahand.kashani-akhavan@epfl.ch>
2012-11-19 21:54:44 +0100

    Added SpringConstraint and Cloth. Cloth.cpp has a redeclaration error that should be fixed witha ifndef.

--- Framework_Linux/src/cloth-simulator/Cloth.cpp
+++ Framework_Linux/src/cloth-simulator/Cloth.cpp
@@ -1,3 +1,7 @@
+#include<vector>
+#include "MassElement.cpp"
+#include "SpringConstraint.cpp"
+
 class Cloth {
 private:
     int numberOfMassElementsOnWidth;
@@ -10,5 +14,20 @@ public:
     Cloth(float p_width, float p_height, int p_numberOfMassElementsOnWidth, int p_numberOfMassElementsOnHeight) :
         numberOfMassElementsOnWidth(p_numberOfMassElementsOnWidth),
         numberOfMassElementsOnHeight(p_numberOfMassElementsOnHeight)
-    {}
-}
+    {
+//
+//        int totalNumberOfMassElements = numberOfMassElementsOnWidth * numberOfMassElementsOnHeight;
+//        massElements.resize(totalNumberOfMassElements);
+//
+//        for(int x = 0; x < numberOfMassElementsOnWidth; x += 1) {
+//            for(int y = 0; y < numberOfMassElementsOnHeight; y += 1) {
+//
+//                float x_position = ((float) x) / ((float) numberOfMassElementsOnWidth);
+//                float y_position = ((float) y) / ((float) numberOfMassElementsOnHeight);
+//                Vector3 massElementPosition = Vector3(x_position, y_position, 0);
+//
+//                massElements[y * numberOfMassElementsOnWidth + x] = MassElement();
+//            }
+//        }
+    }
+};
--- Framework_Linux/src/cloth-simulator/SpringConstraint.cpp
+++ Framework_Linux/src/cloth-simulator/SpringConstraint.cpp
@@ -1,4 +1,5 @@
 #include "../math/Vector3.h"
+#include "MassElement.cpp"
 
 class SpringConstraint {
 
@@ -8,7 +9,7 @@ private:
     MassElement* m2;
 
 public:
-    SpringConstraint(MassElement p_m1, MassElement p_m2) :
+    SpringConstraint(MassElement* p_m1, MassElement* p_m2) :
         m1(p_m1), m2(p_m2)
     {
         Vector3 vectorBetweenM1AndM2 = m2->getPosition() - m1->getPosition();
@@ -20,4 +21,4 @@ public:
         m1->translateByOffset(correctionVectorPerMassElement);
         m2->translateByOffset(-correctionVectorPerMassElement);
     }
-}
+};
