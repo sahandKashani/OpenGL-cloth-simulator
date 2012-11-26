c09763e7200ef469a6af5ef05a075495f0cd4def
scorthes <sebastien.corthesy@epfl.ch>
2012-11-26 23:27:32 +0100

    [SDP] updates

--- AndroidCalendarTest/gen/com/example/androidcalendartest/R.java
+++ AndroidCalendarTest/gen/com/example/androidcalendartest/R.java
@@ -18,11 +18,12 @@ public final class R {
         public static final int begin=0x7f070002;
         public static final int end=0x7f070003;
         public static final int eventTitle=0x7f070000;
-        public static final int getCalendar=0x7f070005;
-        public static final int goTo=0x7f070006;
-        public static final int menu_settings=0x7f070007;
+        public static final int exportToCalendar=0x7f070004;
+        public static final int getCalendar=0x7f070006;
+        public static final int goTo=0x7f070007;
+        public static final int menu_settings=0x7f070008;
         public static final int place=0x7f070001;
-        public static final int writeCalendar=0x7f070004;
+        public static final int writeCalendar=0x7f070005;
     }
     public static final class layout {
         public static final int activity_event_details=0x7f030000;
@@ -37,6 +38,7 @@ public final class R {
         public static final int begin=0x7f040009;
         public static final int end=0x7f04000a;
         public static final int eventTitle=0x7f040006;
+        public static final int exportToCalendar=0x7f04000b;
         public static final int getFromCalendar=0x7f040004;
         public static final int goTo=0x7f040007;
         public static final int menu_settings=0x7f040001;
--- AndroidCalendarTest/src/androidcalendartest/EventDetailsActivity.java
+++ AndroidCalendarTest/src/androidcalendartest/EventDetailsActivity.java
@@ -1,12 +1,19 @@
 package androidcalendartest;
 
+import java.util.Calendar;
+
 import com.example.androidcalendartest.R;
 import com.example.androidcalendartest.R.layout;
 import com.example.androidcalendartest.R.menu;
 
 import android.os.Bundle;
+import android.provider.CalendarContract;
+import android.provider.CalendarContract.Events;
 import android.app.Activity;
+import android.app.Dialog;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
 
 public class EventDetailsActivity extends Activity {
 
@@ -20,5 +27,22 @@ public class EventDetailsActivity extends Activity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_event_details, menu);
         return true;
+    }
+    
+    public void exportToCalendar(View view) {
+    	Calendar beginTime = Calendar.getInstance();
+    	beginTime.set(2012, 0, 19, 7, 30);
+    	Calendar endTime = Calendar.getInstance();
+    	endTime.set(2012, 0, 19, 8, 30);
+    	Intent intent = new Intent(Intent.ACTION_INSERT)
+    	        .setData(Events.CONTENT_URI)
+    	        .putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, beginTime.getTimeInMillis())
+    	        .putExtra(CalendarContract.EXTRA_EVENT_END_TIME, endTime.getTimeInMillis())
+    	        .putExtra(Events.TITLE, "Yoga")
+    	        .putExtra(Events.DESCRIPTION, "Group class")
+    	        .putExtra(Events.EVENT_LOCATION, "The gym")
+    	        .putExtra(Events.AVAILABILITY, Events.AVAILABILITY_BUSY)
+    	        .putExtra(Intent.EXTRA_EMAIL, "rowan@example.com,trevor@example.com");
+    	startActivity(intent);
     }
 }
