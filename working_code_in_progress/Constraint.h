f852126a0634b925e60600208e4ebac1951cdc2b
Sahand Kashani-Akhavan <sahand.kashani-akhavan@epfl.ch>
2012-12-04 15:41:16 +0100

    more file splitting

---
+++ working_code_in_progress/Constraint.h
@@ -0,0 +1,22 @@
+#ifndef CONSTRAINT_H
+#define CONSTRAINT_H
+
+#include "Node.h"
+
+class Constraint
+{
+private:
+    Node* node1;
+    Node* node2;
+    float distanceAtRest;
+
+public:
+    Constraint(Node* n1, Node* n2);
+    Node* getFirstNode();
+    Node* getSecondNode();
+    float getDistanceAtRest();
+    void satisfyConstraint();
+    void draw();
+};
+
+#endif
