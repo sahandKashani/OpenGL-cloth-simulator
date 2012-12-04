3a649580ad1a6f147760fe748e5903dd5a2b28de
Sahand Kashani-Akhavan <sahand.kashani-akhavan@epfl.ch>
2012-12-04 09:42:53 +0100

    - Made pointers to cloth and camera for easy initialization

--- working_code_in_progress/cloth_simulation.cpp
+++ working_code_in_progress/cloth_simulation.cpp
@@ -29,12 +29,12 @@ float farPlane = 1000.0;
 
 // Global variables
 bool drawWireFrameEnabled                 = false;
-bool drawNodesEnabled                     = true;
+bool drawNodesEnabled                     = false;
 bool drawWorldAxisEnabled                 = true;
 bool drawStructuralConstraintsEnabled     = true;
-bool drawShearConstraintsEnabled          = true;
+bool drawShearConstraintsEnabled          = false;
 bool drawStructuralBendConstraintsEnabled = true;
-bool drawShearBendConstraintsEnabled      = true;
+bool drawShearBendConstraintsEnabled      = false;
 
 // camera angle increments in radians
 // increment must be power of 2 for precision reasons
@@ -42,7 +42,7 @@ float angleIncrement = 0.03125; // 2^(-5)
 
 // camera translation increments
 // increment must be power of 2 for precision reasons
-float translationIncrement = 0.0625; // 2^(-4)
+float translationIncrement = 1.0;
 
 // -----------------------------------------------------------------------------
 // Camera class
@@ -876,10 +876,10 @@ void Cloth::drawShearBendConstraints()
 // GLUT setup
 
 // Cloth declaration
-Cloth cloth;
+Cloth* cloth = 0;
 
 // Declare a camera at origin
-Camera camera;
+Camera* camera = 0;
 
 // array which holds pressed status values of all keyboard keys other than the
 // special ones (arrows + F1..F12 keys + home + end ...)
@@ -895,7 +895,7 @@ std::string isEnabled(bool controlVariableEnabled);
 void showHelp();
 void showDrawStatus();
 void showCameraStatus();
-void init();
+void createScene();
 void chooseRenderingMethod();
 void drawWorldAxis();
 void display();
@@ -937,10 +937,10 @@ void applyChanges()
     float duration = getTimeDifference();
 
     // apply all forces to the cloth
-    cloth.applyForces(duration);
+    cloth->applyForces(duration);
 
     // satisfy all constraints of the cloth after forces are applied
-    cloth.satisfyConstraints();
+    cloth->satisfyConstraints();
 
     // redraw the screen
     glutPostRedisplay();
@@ -964,6 +964,12 @@ void showHelp()
     std::cout << "******************************************" << std::endl;
     std::cout << "********           help           ********" << std::endl;
     std::cout << "******************************************" << std::endl;
+
+    std::cout << std::endl;
+
+    std::cout << "ESC: exit cloth simulator" << std::endl;
+
+    std::cout << std::endl;
 
     // help controls
     std::cout << "help controls:" << std::endl;
@@ -1045,18 +1051,21 @@ void showDrawStatus()
 void showCameraStatus()
 {
     std::cout << "camera status:" << std::endl;
-    std::cout << "  camera position      : " << camera.getPosition().toString() << std::endl;
-    std::cout << "  camera view direction: " << camera.getViewDirection().toString() << std::endl;
-    std::cout << "  camera up direction  : " << camera.getUpDirection().toString() << std::endl;
-    std::cout << "  yaw                  : " << camera.getYaw() << " rad" << std::endl;
-    std::cout << "  pitch                : " << camera.getPitch() << " rad" << std::endl;
-    std::cout << "  roll                 : " << camera.getRoll() << " rad" << std::endl;
+    std::cout << "  camera position      : " << camera->getPosition().toString() << std::endl;
+    std::cout << "  camera view direction: " << camera->getViewDirection().toString() << std::endl;
+    std::cout << "  camera up direction  : " << camera->getUpDirection().toString() << std::endl;
+    std::cout << "  yaw                  : " << camera->getYaw() << " rad" << std::endl;
+    std::cout << "  pitch                : " << camera->getPitch() << " rad" << std::endl;
+    std::cout << "  roll                 : " << camera->getRoll() << " rad" << std::endl;
 
     std::cout << std::endl;
 }
 
-void init()
+void createScene()
 {
+    cloth = new Cloth(21, 21);
+    camera = new Camera();
+
     resetCameraPosition();
 
     // TODO : enable later
@@ -1067,20 +1076,21 @@ void init()
     // the value of oldTime will be changed through it's pointer
     gettimeofday(&oldTime, NULL);
 
-    // TODO : fixing 3 top-left and 3 top-right nodes for cape to be held up
-    cloth.setNodeMoveable(0, cloth.getNumberNodesHeight() - 1, false);
-    cloth.setNodeMoveable(cloth.getNumberNodesWidth() - 1, cloth.getNumberNodesHeight() - 1, false);
+    // TODO : fixing cloth at certain points
+    cloth->setNodeMoveable(0, cloth->getNumberNodesHeight() - 1, false);
+    cloth->setNodeMoveable(cloth->getNumberNodesWidth() - 1, cloth->getNumberNodesHeight() - 1, false);
+    cloth->setNodeMoveable(cloth->getNumberNodesWidth() / 2, cloth->getNumberNodesHeight() - 1, false);
 
     // TODO : find suitable values
     // gravity
-    Vector3 gravity(0.0, -0.1, 0.0);
+    Vector3 gravity(0.0, -1.0, 0.0);
 
     // TODO : find suitable values
     // wind
-    Vector3 wind(0.0, 0.0, 0.1);
+    Vector3 wind(0.0, 0.0, 0.5);
 
-    cloth.addForce(gravity);
-    cloth.addForce(wind);
+    cloth->addForce(gravity);
+    cloth->addForce(wind);
 
     // clear keyboard press status
     initializeKeyboardStatus();
@@ -1089,11 +1099,12 @@ void init()
 void resetCameraPosition()
 {
     // reset the camera
-    camera = Camera();
+    delete camera;
+    camera = new Camera();
 
     // move camera back
-    float cameraZ = 1.75 * std::max(cloth.getNumberNodesWidth(), cloth.getNumberNodesHeight());
-    camera.translate(Vector3(0.0, 0.0, cameraZ));
+    float cameraZ = 1.75 * std::max(cloth->getNumberNodesWidth(), cloth->getNumberNodesHeight());
+    camera->translate(Vector3(0.0, 0.0, cameraZ));
 }
 
 // determines if a wireframe is to be drawn, or a textured version
@@ -1165,9 +1176,9 @@ void display()
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
 
-    Vector3 cameraPosition = camera.getPosition();
-    Vector3 cameraViewDirection = camera.getViewDirection();
-    Vector3 cameraUpDirection = camera.getUpDirection();
+    Vector3 cameraPosition = camera->getPosition();
+    Vector3 cameraViewDirection = camera->getViewDirection();
+    Vector3 cameraUpDirection = camera->getUpDirection();
 
     // all perspectives are already calculated and stored, so just position
     // the camera where it is supposed to be
@@ -1188,7 +1199,7 @@ void display()
     drawWorldAxis();
 
     // draw cloth
-    cloth.draw();
+    cloth->draw();
 
     // swap buffers needed for double buffering
     glutSwapBuffers();
@@ -1213,48 +1224,48 @@ void applyContinuousKeyboardCommands()
             {
                 // world axis camera controls
                 case 'a':
-                    camera.rotateAroundYAxisWorld(-angleIncrement);
+                    camera->rotateAroundYAxisWorld(-angleIncrement);
                     break;
                 case 'd':
-                    camera.rotateAroundYAxisWorld(angleIncrement);
+                    camera->rotateAroundYAxisWorld(angleIncrement);
                     break;
                 case 's':
-                    camera.rotateAroundXAxisWorld(-angleIncrement);
+                    camera->rotateAroundXAxisWorld(-angleIncrement);
                     break;
                 case 'w':
-                    camera.rotateAroundXAxisWorld(angleIncrement);
+                    camera->rotateAroundXAxisWorld(angleIncrement);
                     break;
                 case 'q':
-                    camera.rotateAroundZAxisWorld(-angleIncrement);
+                    camera->rotateAroundZAxisWorld(-angleIncrement);
                     break;
                 case 'e':
-                    camera.rotateAroundZAxisWorld(angleIncrement);
+                    camera->rotateAroundZAxisWorld(angleIncrement);
                     break;
 
                 // yaw, pitch and roll camera controls
                 case 'k':
                     // turn camera "down" one notch
-                    camera.rotateAroundXAxisObject(angleIncrement);
+                    camera->rotateAroundXAxisObject(angleIncrement);
                     break;
                 case 'i':
                     // turn camera "up" one notch
-                    camera.rotateAroundXAxisObject(-angleIncrement);
+                    camera->rotateAroundXAxisObject(-angleIncrement);
                     break;
                 case 'j':
                     // turn camera "left" one notch
-                    camera.rotateAroundYAxisObject(angleIncrement);
+                    camera->rotateAroundYAxisObject(angleIncrement);
                     break;
                 case 'l':
                     // turn camera "right" one notch
-                    camera.rotateAroundYAxisObject(-angleIncrement);
+                    camera->rotateAroundYAxisObject(-angleIncrement);
                     break;
                 case 'u':
                     // tilt camera "left" one notch
-                    camera.rotateAroundZAxisObject(-angleIncrement);
+                    camera->rotateAroundZAxisObject(-angleIncrement);
                     break;
                 case 'o':
                     // tilt camera "right" one notch
-                    camera.rotateAroundZAxisObject(angleIncrement);
+                    camera->rotateAroundZAxisObject(angleIncrement);
                     break;
 
                 // camera translation controls
@@ -1262,31 +1273,33 @@ void applyContinuousKeyboardCommands()
                     // translate camera left
                     // calculate "x" axis of camera with a cross product between
                     // it's upDirection and viewDirection
-                    camera.translate(camera.getUpDirection().cross(camera.getViewDirection()).normalize() * translationIncrement);
+                    // FIXME : translates in wrong direction
+                    camera->translate(camera->getUpDirection().cross(camera->getViewDirection()).normalize() * translationIncrement);
                     break;
                 case 'h':
                     // translate camera right
                     // calculate "x" axis of camera with a cross product between
                     // it's upDirection and viewDirection
-                    camera.translate(camera.getViewDirection().cross(camera.getUpDirection()).normalize() * translationIncrement);
+                    // FIXME : translates in wrong direction
+                    camera->translate(camera->getViewDirection().cross(camera->getUpDirection()).normalize() * translationIncrement);
                     break;
                 case 't':
                     // translate camera up
-                    camera.translate(camera.getUpDirection().normalize() * translationIncrement);
+                    camera->translate(camera->getUpDirection().normalize() * translationIncrement);
                     break;
                 case 'g':
                     // translate camera down
-                    camera.translate(-camera.getUpDirection().normalize() * translationIncrement);
+                    camera->translate(-camera->getUpDirection().normalize() * translationIncrement);
                     break;
                 case 'r':
                     // translate camera back
                     // FIXME : gets stuck if advance too much, since the vector's length becomes 0.0, and normalize throws assertion error
-                    camera.translate(camera.getViewDirection().normalize() * translationIncrement);
+                    camera->translate(camera->getViewDirection().normalize() * translationIncrement);
                     break;
                 case 'z':
                     // translate camera front
                     // FIXME : gets stuck if advance too much, since the vector's length becomes 0.0, and normalize throws assertion error
-                    camera.translate(-camera.getViewDirection().normalize() * translationIncrement);
+                    camera->translate(-camera->getViewDirection().normalize() * translationIncrement);
                     break;
 
                 default:
@@ -1302,6 +1315,9 @@ void normalKeyboard(unsigned char key, int x, int y)
     {
         // ESC control
         case 27:
+            // clean up the cloth and camera before exiting
+            delete camera;
+            delete cloth;
             exit(0);
             break;
 
@@ -1393,11 +1409,10 @@ int main(int argc, char** argv)
 
     glutCreateWindow("Cloth Simulation");
 
-    init();
+    createScene();
+
     glutDisplayFunc(display);
-
     glutIdleFunc(applyChanges);
-
     glutReshapeFunc(reshape);
 
     // disable keyboard repeat, because we will use variables for continuous animation
