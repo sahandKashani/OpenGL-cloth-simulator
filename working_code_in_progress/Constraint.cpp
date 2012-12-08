98c8e2ff96583483f8fd48e56f400315a9d77e48
Sahand Kashani-Akhavan <sahand.kashani-akhavan@epfl.ch>
2012-12-08 11:23:57 +0100

    - Added if(enabled) to satisfyConstraint

--- working_code_in_progress/Constraint.cpp
+++ working_code_in_progress/Constraint.cpp
@@ -45,32 +45,35 @@ float Constraint::getDistanceAtRest()
 
 void Constraint::satisfyConstraint()
 {
-    Vector3 vectorFromNode1ToNode2 = node2->getPosition() - node1->getPosition();
-    float currentDistance = vectorFromNode1ToNode2.length();
-    float restToCurrentDistanceRatio = distanceAtRest / currentDistance;
-    Vector3 correctionVectorFromNode1ToNode2 = vectorFromNode1ToNode2 * (1 - restToCurrentDistanceRatio);
+    if(enabled)
+    {
+        Vector3 vectorFromNode1ToNode2 = node2->getPosition() - node1->getPosition();
+        float currentDistance = vectorFromNode1ToNode2.length();
+        float restToCurrentDistanceRatio = distanceAtRest / currentDistance;
+        Vector3 correctionVectorFromNode1ToNode2 = vectorFromNode1ToNode2 * (1 - restToCurrentDistanceRatio);
 
-    bool node1Moveable = node1->isMoveable();
-    bool node2Moveable = node2->isMoveable();
+        bool node1Moveable = node1->isMoveable();
+        bool node2Moveable = node2->isMoveable();
 
-    if(node1Moveable && node2Moveable)
-    {
-        // move both nodes towards each other by 0.5 * correctionVectorFromNode1ToNode2
-        // positive direction for node1 (correction vector goes from node1 to node 2)
-        // therefore, negative direction for node2
-        node1->translate(0.5 * correctionVectorFromNode1ToNode2);
-        node2->translate(-0.5 * correctionVectorFromNode1ToNode2);
-    }
-    else if(node1Moveable && !node2Moveable)
-    {
-        // move node1 towards node2 by +1.0 * correctionVectorFromNode1ToNode2
-        // (positive sign, because the correction vector is pointing towards node2)
-        node1->translate(correctionVectorFromNode1ToNode2);
-    }
-    else if(!node1Moveable && node2Moveable)
-    {
-        // move node2 towards node1 by -1.0 * correctionVectorFromNode1ToNode2
-        // (negative sign, because the correction vector is pointing towards node2)
-        node2->translate(-correctionVectorFromNode1ToNode2);
+        if(node1Moveable && node2Moveable)
+        {
+            // move both nodes towards each other by 0.5 * correctionVectorFromNode1ToNode2
+            // positive direction for node1 (correction vector goes from node1 to node 2)
+            // therefore, negative direction for node2
+            node1->translate(0.5 * correctionVectorFromNode1ToNode2);
+            node2->translate(-0.5 * correctionVectorFromNode1ToNode2);
+        }
+        else if(node1Moveable && !node2Moveable)
+        {
+            // move node1 towards node2 by +1.0 * correctionVectorFromNode1ToNode2
+            // (positive sign, because the correction vector is pointing towards node2)
+            node1->translate(correctionVectorFromNode1ToNode2);
+        }
+        else if(!node1Moveable && node2Moveable)
+        {
+            // move node2 towards node1 by -1.0 * correctionVectorFromNode1ToNode2
+            // (negative sign, because the correction vector is pointing towards node2)
+            node2->translate(-correctionVectorFromNode1ToNode2);
+        }
     }
 }
